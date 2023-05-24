c8ee776d7503c70c33c61342196eae673d742292
chandra <chandrapenugonda655@gmail.com>
2023-05-24 08:54:39 +0530

    fastify setup

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import compression from '@fastify/compress';
+import fastifyCsrf from '@fastify/csrf-protection';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.register(compression);
+  await app.register(fastifyCsrf);
+  await app.listen(3000);
+}
+bootstrap();
